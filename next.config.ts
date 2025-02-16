aea18c9dfeb156f75bb8e82b2c9089c123c884bb
SAMUEopps <samuelhope805@gmail.com>
2025-02-16 15:30:48 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["i.pinimg.com", "res.cloudinary.com"], // Add res.cloudinary.com here
+  },
 };
 
 export default nextConfig;
